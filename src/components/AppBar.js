03da8bab3d6ee53008a52e6db15f620d456e2b7f
manish <ms5978307@gmail.com>
2023-01-30 11:42:06 +0530

    added app bar

---
+++ src/components/AppBar.js
@@ -0,0 +1,47 @@
+const AppBar = ({ arrLen, range, setArrLen, setRange, setIsSorting }) => {
+    return (
+        <header>
+            <section>
+                <label htmlFor="array-length">Array Length</label>
+                <input
+                    type="number"
+                    name="array-length"
+                    value={arrLen}
+                    onChange={(e) => setArrLen(e.target.value)}
+                />
+            </section>
+            <section>
+                <label htmlFor="array-length">Minimum</label>
+                <input
+                    type="number"
+                    name="min"
+                    value={range.min}
+                    onChange={(e) =>
+                        setRange((val) => ({ ...val, min: e.target.value }))
+                    }
+                />
+            </section>
+            <section>
+                <label htmlFor="array-length">Maximum</label>
+                <input
+                    type="number"
+                    name="max"
+                    value={range.max}
+                    onChange={(e) =>
+                        setRange((val) => ({ ...val, max: e.target.value }))
+                    }
+                />
+            </section>
+            <button
+                onClick={() => {
+                    setIsSorting(true);
+                    console.log("true");
+                }}
+            >
+                Sort
+            </button>
+        </header>
+    );
+};
+
+export default AppBar;
